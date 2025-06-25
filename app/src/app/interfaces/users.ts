15d3c355f5c95af5df52a36a6b035cd5311bedfc
muhammadathallahaidane <talaaidane@gmail.com>
2025-06-25 19:30:03 +0700

    add registration routes and user models interfaces

---
+++ app/src/app/interfaces/users.ts
@@ -0,0 +1,12 @@
+export interface IUsers {
+    name: string,
+    username: string,
+    email: string,
+    password: string,
+    gender: string
+}
+
+export interface ILogin {
+    email: string,
+    password: string
+}
