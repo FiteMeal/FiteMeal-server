3d72c2cf06bf70705ef777cf26057dec454ddc91
muhammadathallahaidane <talaaidane@gmail.com>
2025-06-27 19:15:45 +0700

    refactor: response midtrans-token and error handling

--- app/src/app/api/midtrans-token/route.ts
+++ app/src/app/api/midtrans-token/route.ts
@@ -1,3 +1,4 @@
+import { CustomError } from "@/db/helpers/CustomError";
 import errorHandler from "@/db/helpers/errorHandler";
 
 export async function POST(req: Request) {
@@ -14,19 +15,19 @@ export async function POST(req: Request) {
           id: "ITEM1",
           price: amount,
           quantity: 1,
-          name: "FiteMeal Premium Account",
-          brand: "FiteMeal",
-          category: "Health",
-          merchant_name: "Fitemeal",
-          url: "http://toko/toko1?item=abc",
+          name: "FiteMeal Premium Plan",
         },
       ],
+      customer_details: {
+        last_name: "ini harusnya name user yang login",
+        email: "iniemailuseryanglogin@mail.com",
+      },
     };
 
     const authString = Buffer.from(
       process.env.MIDTRANS_SERVER_KEY + ":"
     ).toString("base64");
-    const data = await fetch(
+    const resp = await fetch(
       "https://app.sandbox.midtrans.com/snap/v1/transactions",
       {
         method: "POST",
@@ -39,9 +40,24 @@ export async function POST(req: Request) {
       }
     );
 
-    const midtransToken = await data.json();
+    
+    const midtransResp = await resp.json();
+    console.log(midtransResp);
+    
+    if (!resp.ok) {
+      const errorMessage = midtransResp.error_messages
+        ? midtransResp.error_messages.join(", ")
+        : midtransResp.message || "Midtrans API Error";
 
-    return Response.json({ midtransToken }, { status: 201 });
+      throw new CustomError(`Midtrans Error: ${errorMessage}`, resp.status);
+    }
+    return Response.json(
+      {
+        midtransToken: midtransResp.token,
+        paymentMethodLink: midtransResp.redirect_url,
+      },
+      { status: 201 }
+    );
   } catch (error) {
     console.log(error);
     const { message, status } = errorHandler(error);
