1e1846ad31f84f8e35677c6703aa4a178c0ef18d
diotaufiq <diotfq@gmail.com>
2025-06-28 22:51:53 +0700

    feat: add exercise generation functionality and related interfaces

---
+++ app/src/db/models/dataExcercise.ts
@@ -0,0 +1,8 @@
+import { ExercisePlan } from "@/app/interfaces/excercise";
+import { Model } from "mongoloquent";
+
+export default class dataExcercise extends Model<ExercisePlan>{
+
+    static $schema: dataExcercise;
+    protected $collection: string = 'plans'
+}
